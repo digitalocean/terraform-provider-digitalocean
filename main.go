0b01241c917dd6fc94f0f40ec3f0329b905491ea
Jake Champlin <jake.champlin.27@gmail.com>
2017-06-06 11:45:18 -0400

    Initial transfer of provider code

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-digitalocean/digitalocean"
+)
+
 func main() {
-	/*plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: opc.Provider})*/
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: digitalocean.Provider})
 }
