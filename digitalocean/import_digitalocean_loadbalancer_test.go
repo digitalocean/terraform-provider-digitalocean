cf6c8ad9f98365f4d142520a36a1f5c36aed47cd
Jonny Ford <jonny@instasupply.com>
2018-01-04 12:08:16 +0000

    Added random int for test configs

--- digitalocean/import_digitalocean_loadbalancer_test.go
+++ digitalocean/import_digitalocean_loadbalancer_test.go
@@ -3,19 +3,21 @@ package digitalocean
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccDigitalOceanLoadBalancer_importBasic(t *testing.T) {
 	resourceName := "digitalocean_loadbalancer.foobar"
+	rInt := acctest.RandInt()
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckDigitalOceanLoadBalancerDestroy,
+		CheckDestroy: testAccCheckDigitalOceanLoadbalancerDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckDigitalOceanLoadbalancerConfig_basic,
+				Config: testAccCheckDigitalOceanLoadbalancerConfig_basic(rInt),
 			},
 
 			{
