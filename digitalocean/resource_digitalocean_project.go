51a668c89c69818d0729c92386c41fca0c1fc40a
Trent Rosenbaum <trent.rosenbaum@opencredo>
2019-04-17 10:10:56 +0100

    Updates the Project resource to manage its basic fields.

--- digitalocean/resource_digitalocean_project.go
+++ digitalocean/resource_digitalocean_project.go
@@ -1,8 +1,13 @@
 package digitalocean
 
 import (
+	"context"
+	"fmt"
+	"github.com/digitalocean/godo"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
+	"log"
+	"strings"
 )
 
 func resourceDigitalOceanProject() *schema.Resource {
@@ -17,40 +22,148 @@ func resourceDigitalOceanProject() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "the human-readable name for the project",
+				ValidateFunc: validation.All(
+					validation.NoZeroValues,
+					validation.StringLenBetween(1, 175),
+				),
+			},
+			"description": {
 				Type:         schema.TypeString,
-				Required:     true,
-				ForceNew:     true,
-				ValidateFunc: validation.NoZeroValues,
+				Optional:     true,
+				Default:      "",
+				Description:  "the descirption of the project",
+				ValidateFunc: validation.StringLenBetween(0, 255),
 			},
 			"purpose": {
 				Type:         schema.TypeString,
-				Required:     true,
-				ValidateFunc: validation.NoZeroValues,
+				Optional:     true,
+				Default:      "Web Application",
+				Description:  "the purpose of the project",
+				ValidateFunc: validation.StringLenBetween(0, 255),
+			},
+			"environment": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "Development",
+				Description:  "the environment of the project's resources",
+				ValidateFunc: validation.StringInSlice([]string{"development", "staging", "production"}, true),
+			},
+			"owner_uuid": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "the unique universal identifier of the project owner.",
+			},
+			"owner_id": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "the id of the project owner.",
+			},
+			"created_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "the date and time when the project was created, (ISO8601)",
+			},
+			"updated_at": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "the date and time when the project was last updated, (ISO8601)",
 			},
 		},
 	}
 }
 
 func resourceDigitalOceanProjectCreate(d *schema.ResourceData, meta interface{}) error {
-	//client := meta.(*CombinedConfig).godoClient()
+	client := meta.(*CombinedConfig).godoClient()
 
-	return resourceDigitalOceanCertificateRead(d, meta)
+	projectRequest := &godo.CreateProjectRequest{
+		Name:    d.Get("name").(string),
+		Purpose: d.Get("purpose").(string),
+	}
+
+	if v, ok := d.GetOk("description"); ok {
+		projectRequest.Description = v.(string)
+	}
+
+	if v, ok := d.GetOk("environment"); ok {
+		projectRequest.Environment = v.(string)
+	}
+
+	log.Printf("[DEBUG] Project create request: %#v", projectRequest)
+	project, _, err := client.Projects.Create(context.Background(), projectRequest)
+
+	if err != nil {
+		return fmt.Errorf("Error creating Project: %s", err)
+	}
+
+	d.SetId(project.ID)
+	log.Printf("[INFO] Project created, ID: %s", d.Id())
+
+	return resourceDigitalOceanProjectRead(d, meta)
 }
 
 func resourceDigitalOceanProjectRead(d *schema.ResourceData, meta interface{}) error {
-	//client := meta.(*CombinedConfig).godoClient()
+	client := meta.(*CombinedConfig).godoClient()
 
-	return nil
+	project, resp, err := client.Projects.Get(context.Background(), d.Id())
+
+	if err != nil {
+		if resp != nil && resp.StatusCode == 404 {
+			log.Printf("[DEBUG] Project  (%s) was not found - removing from state", d.Id())
+			d.SetId("")
+		}
+
+		return fmt.Errorf("Error reading Project: %s", err)
+	}
+
+	d.Set("id", project.ID)
+	d.Set("name", project.Name)
+	d.Set("purpose", strings.TrimPrefix(project.Purpose, "Other: "))
+	d.Set("description", project.Description)
+	d.Set("environment", project.Environment)
+	d.Set("is_default", project.IsDefault)
+	d.Set("owner_uuid", project.OwnerUUID)
+	d.Set("owner_id", project.OwnerID)
+	d.Set("created_at", project.CreatedAt)
+	d.Set("updated_at", project.UpdatedAt)
+
+	return err
 }
 
 func resourceDigitalOceanProjectUpdate(d *schema.ResourceData, meta interface{}) error {
-	//client := meta.(*CombinedConfig).godoClient()
+	client := meta.(*CombinedConfig).godoClient()
 
-	return resourceDigitalOceanCertificateRead(d, meta)
+	projectRequest := &godo.UpdateProjectRequest{
+		Name:        d.Get("name"),
+		Description: d.Get("description"),
+		Purpose:     d.Get("purpose"),
+		Environment: d.Get("environment"),
+		IsDefault:   d.Get("is_default"),
+	}
+
+	_, _, err := client.Projects.Update(context.Background(), d.Id(), projectRequest)
+	if err != nil {
+		return fmt.Errorf("Error updating Project: %s", err)
+	}
+
+	log.Printf("[INFO] Updated Project")
+
+	return resourceDigitalOceanProjectRead(d, meta)
 }
 
 func resourceDigitalOceanProjectDelete(d *schema.ResourceData, meta interface{}) error {
-	//client := meta.(*CombinedConfig).godoClient()
+	client := meta.(*CombinedConfig).godoClient()
+	resourceId := d.Id()
+
+	_, err := client.Projects.Delete(context.Background(), resourceId)
+	if err != nil {
+		return fmt.Errorf("Error deleteing Project %s", err)
+	}
+
+	d.SetId("")
+	log.Printf("[INFO] Project deleted, ID: %s", resourceId)
 
 	return nil
 }
--- digitalocean/resource_digitalocean_project_test.go
+++ digitalocean/resource_digitalocean_project_test.go
@@ -1,11 +1,202 @@
 package digitalocean
 
 import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 	"testing"
 )
 
-func TestAccDigitalOceanProject_Basic(t *testing.T) {
+func TestAccDigitalOceanProject_CreateWithDefaults(t *testing.T) {
 
+	expectedName := generateProjectName()
+	createConfig := fixtureCreateWithDefaults(expectedName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDigitalOceanProjectDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: createConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDigitalOceanProjectExists("digitalocean_project.myproj"),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "name", expectedName),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "description", ""),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "purpose", "Web Application"),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "environment", "Development"),
+					resource.TestCheckResourceAttrSet("digitalocean_project.myproj", "id"),
+					resource.TestCheckResourceAttrSet("digitalocean_project.myproj", "owner_uuid"),
+					resource.TestCheckResourceAttrSet("digitalocean_project.myproj", "owner_id"),
+					resource.TestCheckResourceAttrSet("digitalocean_project.myproj", "created_at"),
+					resource.TestCheckResourceAttrSet("digitalocean_project.myproj", "updated_at"),
+				),
+			},
+		},
+	})
 }
 
-const testAccCheckDigitalOceanProjectConfig_basic = ``
+func TestAccDigitalOceanProject_CreateWithInitialValues(t *testing.T) {
+
+	expectedName := generateProjectName()
+	expectedDescription := "A simple project for a web app."
+	expectedPurpose := "My Basic Web App"
+	expectedEnvironment := "Production"
+
+	createConfig := fixtureCreateWithInitialValues(expectedName, expectedDescription,
+		expectedPurpose, expectedEnvironment)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDigitalOceanProjectDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: createConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDigitalOceanProjectExists("digitalocean_project.myproj"),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "name", expectedName),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "description", expectedDescription),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "purpose", expectedPurpose),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "environment", expectedEnvironment),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDigitalOceanProject_UpdateWithInitialValues(t *testing.T) {
+
+	expectedName := generateProjectName()
+	expectedDesc := "A simple project for a web app."
+	expectedPurpose := "My Basic Web App"
+	expectedEnv := "Production"
+
+	createConfig := fixtureCreateWithInitialValues(expectedName, expectedDesc,
+		expectedPurpose, expectedEnv)
+
+	expectedUpdateName := generateProjectName()
+	expectedUpdateDesc := "A simple project for Beta testing."
+	expectedUpdatePurpose := "MyWeb App, (Beta)"
+	expectedUpdateEnv := "Staging"
+
+	updateConfig := fixtureUpdateWithValues(expectedUpdateName, expectedUpdateDesc,
+		expectedUpdatePurpose, expectedUpdateEnv)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDigitalOceanProjectDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: createConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDigitalOceanProjectExists("digitalocean_project.myproj"),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "name", expectedName),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "description", expectedDesc),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "purpose", expectedPurpose),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "environment", expectedEnv),
+				),
+			},
+			{
+				Config: updateConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDigitalOceanProjectExists("digitalocean_project.myproj"),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "name", expectedUpdateName),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "description", expectedUpdateDesc),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "purpose", expectedUpdatePurpose),
+					resource.TestCheckResourceAttr(
+						"digitalocean_project.myproj", "environment", expectedUpdateEnv),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDigitalOceanProjectDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*CombinedConfig).godoClient()
+
+	for _, rs := range s.RootModule().Resources {
+
+		if rs.Type != "digitalocean_project" {
+			continue
+		}
+
+		_, _, err := client.Projects.Get(context.Background(), rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("Project resource still exists")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckDigitalOceanProjectExists(resource string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*CombinedConfig).godoClient()
+
+		rs, ok := s.RootModule().Resources[resource]
+
+		if !ok {
+			return fmt.Errorf("Not found: %s", resource)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID set for resource: %s", resource)
+		}
+
+		foundProject, _, err := client.Projects.Get(context.Background(), rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		if foundProject.ID != rs.Primary.ID {
+			return fmt.Errorf("Resource not found: %s : %s", resource, rs.Primary.ID)
+		}
+
+		return nil
+	}
+}
+
+func generateProjectName() string {
+	return fmt.Sprintf("tf-proj-test-%d", acctest.RandInt())
+}
+
+func fixtureCreateWithDefaults(name string) string {
+	return fmt.Sprintf(`
+		resource "digitalocean_project" "myproj" {
+			name = "%s"
+		}`, name)
+}
+
+func fixtureUpdateWithValues(name, description, purpose, environment string) string {
+	return fixtureCreateWithInitialValues(name, description, purpose, environment)
+}
+
+func fixtureCreateWithInitialValues(name, description, purpose, environment string) string {
+	return fmt.Sprintf(`
+		resource "digitalocean_project" "myproj" {
+			name = "%s"
+			description = "%s"
+			purpose = "%s"
+			environment = "%s"
+		}`, name, description, purpose, environment)
+}
