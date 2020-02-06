c74850d5839b6d8081a7e2c2e9c0b644240a0f0f
Tom Dyas <tom.dyas@gmail.com>
2020-02-06 01:23:00 -0800

    add a ImportStateCheck function in an attempt to get test to pass

--- digitalocean/import_digitalocean_kubernetes_node_pool_test.go
+++ digitalocean/import_digitalocean_kubernetes_node_pool_test.go
@@ -5,10 +5,13 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccDigitalOceanKubernetesNodePool_Import(t *testing.T) {
-	rName := randomTestName()
+	testName1 := randomTestName()
+	testName2 := randomTestName()
+
 	config := fmt.Sprintf(`
 resource "digitalocean_kubernetes_cluster" "foobar" {
   name = "%s"
@@ -28,7 +31,7 @@ resource digitalocean_kubernetes_node_pool "barfoo" {
   size = "s-1vcpu-2gb"
   node_count = 1
 }
-`, rName, testClusterVersion16, rName)
+`, testName1, testClusterVersion16, testName2)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -39,11 +42,25 @@ resource digitalocean_kubernetes_node_pool "barfoo" {
 				Config: config,
 			},
 			{
-				ResourceName:      "digitalocean_kubernetes_node_pool.barfoo",
-				ImportState:       true,
-				ImportStateVerify: true,
-				ImportStateVerifyIgnore: []string{
-					"node_count", // because import test failed before DO had started the node in pool
+				ResourceName: "digitalocean_kubernetes_cluster.foobar",
+				ImportState:  true,
+				ImportStateCheck: func(s []*terraform.InstanceState) error {
+					if len(s) != 2 {
+						return fmt.Errorf("expected 2 states: %#v", s)
+					}
+					clusterState, nodePoolState := s[0], s[1]
+
+					if clusterState.Attributes["name"] != testName1 {
+						return fmt.Errorf("expected name attribute for cluster to match: expected=%s, actual=%s",
+							clusterState.Attributes["name"], testName1)
+					}
+
+					if nodePoolState.Attributes["name"] != testName2 {
+						return fmt.Errorf("expected name attribute for node pool to match: expected=%s, actual=%s",
+							nodePoolState.Attributes["name"], testName2)
+					}
+
+					return nil
 				},
 			},
 		},
