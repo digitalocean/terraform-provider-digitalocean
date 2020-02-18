51044dd01bddcecbb4cd1e5b1ee18e9493ec124b
Tom Dyas <tom.dyas@gmail.com>
2020-02-18 15:24:50 -0800

    add test for missing slug for digitalocean_region data source

--- digitalocean/datasource_digitalocean_region_test.go
+++ digitalocean/datasource_digitalocean_region_test.go
@@ -1,6 +1,7 @@
 package digitalocean
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -25,6 +26,24 @@ data "digitalocean_region" "lon1" {
 					resource.TestCheckResourceAttrSet("data.digitalocean_region.lon1", "sizes.#"),
 					resource.TestCheckResourceAttrSet("data.digitalocean_region.lon1", "features.#"),
 				),
+			},
+		},
+	})
+}
+
+func TestAccDigitalOceanRegion_MissingSlug(t *testing.T) {
+	config := `
+data "digitalocean_region" "xyz5" {
+	slug = "xyz5"
+}
+`
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				ExpectError: regexp.MustCompile("Region does not exist: xyz5"),
 			},
 		},
 	})
