12af2977695742aadfb1734cbc91f3f881fcd1d8
Tommy Brunn <tommy.brunn@gmail.com>
2020-03-23 15:50:32 +0100

    Validate connection pool name (#403)
    * Validate length of database connection pool name
    
    DO currently returns a 500 if the database connection pool name is longer than 60 characters, and their
    documentation states that it should be between 3 and 63 characters long.
    
    * Sort imports
    
    * Fix typo

--- digitalocean/resource_digitalocean_project.go
+++ digitalocean/resource_digitalocean_project.go
@@ -3,11 +3,12 @@ package digitalocean
 import (
 	"context"
 	"fmt"
+	"log"
+	"strings"
+
 	"github.com/digitalocean/godo"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
-	"log"
-	"strings"
 )
 
 func resourceDigitalOceanProject() *schema.Resource {
@@ -34,7 +35,7 @@ func resourceDigitalOceanProject() *schema.Resource {
 				Type:         schema.TypeString,
 				Optional:     true,
 				Default:      "",
-				Description:  "the descirption of the project",
+				Description:  "the description of the project",
 				ValidateFunc: validation.StringLenBetween(0, 255),
 			},
 			"purpose": {
