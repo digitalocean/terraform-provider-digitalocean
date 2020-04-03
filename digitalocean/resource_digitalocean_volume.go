6366518fc2812dc02d3a379f0e50151c3b3ae8cd
Matteo Valentini <matteo.valentini@nethesis.it>
2020-04-03 10:12:45 +0200

    support old volumes created with a dot in the name
    Related PR #404

--- digitalocean/resource_digitalocean_volume.go
+++ digitalocean/resource_digitalocean_volume.go
@@ -37,9 +37,11 @@ func resourceDigitalOceanVolume() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
-				// Underscores (_) allowed for backwards compatibility.
-				// See: https://github.com/terraform-providers/terraform-provider-digitalocean/pull/404
-				ValidateFunc: validation.StringMatch(regexp.MustCompile(`^[a-z0-9\-_]+$`), "Names must be lowercase and be composed only of numbers, letters and \"-\"."),
+				// Underscores (_) and dots (.) are allowed for backwards compatibility.
+				// See:
+				//   * https://github.com/terraform-providers/terraform-provider-digitalocean/pull/404
+				//   * https://github.com/terraform-providers/terraform-provider-digitalocean/pull/406
+				ValidateFunc: validation.StringMatch(regexp.MustCompile(`^[a-z0-9\-_.]+$`), "Names must be lowercase and be composed only of numbers, letters and \"-\"."),
 			},
 			"urn": {
 				Type:        schema.TypeString,
