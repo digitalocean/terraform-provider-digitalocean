6047fcb663f14b5177cbefc862ba0763ae54985e
Tom Dyas <tom.dyas@gmail.com>
2020-04-05 00:35:25 -0700

    remove moot comment

--- digitalocean/resource_digitalocean_spaces_bucket_object.go
+++ digitalocean/resource_digitalocean_spaces_bucket_object.go
@@ -120,9 +120,6 @@ func resourceDigitalOceanSpacesBucketObject() *schema.Resource {
 
 			"etag": {
 				Type: schema.TypeString,
-				// This will conflict with SSE-C and SSE-KMS encryption and multi-part upload
-				// if/when it's actually implemented. The Etag then won't match raw-file MD5.
-				// See http://docs.aws.amazon.com/AmazonS3/latest/API/RESTCommonResponseHeaders.html
 				Optional: true,
 				Computed: true,
 			},
