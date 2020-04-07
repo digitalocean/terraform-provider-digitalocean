132f587f34320361eaaaabe5df068c57cd58ac6a
Matteo Valentini <matteo.valentini@nethesis.it>
2020-04-07 16:14:57 +0200

    Remove the validation on volume name (#406)
    Let terraform check only if name exist and let the DigitalOCean APIs
    validate the composition of the name.
    
    Reverts: https://github.com/terraform-providers/terraform-provider-digitalocean/pull/386

--- digitalocean/resource_digitalocean_volume.go
+++ digitalocean/resource_digitalocean_volume.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"regexp"
 	"strings"
 
 	"github.com/digitalocean/godo"
@@ -34,12 +33,10 @@ func resourceDigitalOceanVolume() *schema.Resource {
 			},
 
 			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-				// Underscores (_) allowed for backwards compatibility.
-				// See: https://github.com/terraform-providers/terraform-provider-digitalocean/pull/404
-				ValidateFunc: validation.StringMatch(regexp.MustCompile(`^[a-z0-9\-_]+$`), "Names must be lowercase and be composed only of numbers, letters and \"-\"."),
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.NoZeroValues,
 			},
 			"urn": {
 				Type:        schema.TypeString,
