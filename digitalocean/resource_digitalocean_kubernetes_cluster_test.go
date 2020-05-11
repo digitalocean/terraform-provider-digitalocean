28e0a0af385a3ec50bbdd880c3f68b0b64f48860
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2020-05-11 11:20:38 -0400

    Bump k8s test version slugs.

--- digitalocean/resource_digitalocean_kubernetes_cluster_test.go
+++ digitalocean/resource_digitalocean_kubernetes_cluster_test.go
@@ -13,8 +13,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-const testClusterVersion15 = "1.15.9-do.2"
-const testClusterVersion16 = "1.16.6-do.2"
+const testClusterVersion15 = "1.15.11-do.0"
+const testClusterVersion16 = "1.16.8-do.0"
 
 func TestAccDigitalOceanKubernetesCluster_Basic(t *testing.T) {
 	t.Parallel()
