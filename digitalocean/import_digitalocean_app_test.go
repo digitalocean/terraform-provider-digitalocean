88b33a8171e14de3be42b2e5f20a5a2376b5992b
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2020-10-01 18:00:52 -0400

    Add import test.

---
+++ digitalocean/import_digitalocean_app_test.go
@@ -0,0 +1,29 @@
+package digitalocean
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDigitalOceanApp_importBasic(t *testing.T) {
+	resourceName := "digitalocean_app.foobar"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDigitalOceanCertificateDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccCheckDigitalOceanAppConfig_basic, randomTestName()),
+			},
+
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
