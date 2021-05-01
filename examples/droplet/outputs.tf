cf0714649e1d1188ed6be28a09cea4ca3ea9d551
Mads Jensen <mads.jensen@eficode.com>
2021-05-01 20:32:38 +0200

    Removed interpolation

--- examples/droplet/outputs.tf
+++ examples/droplet/outputs.tf
@@ -1,11 +1,11 @@
 output "IPv4" {
-  value = "${digitalocean_droplet.mywebserver.ipv4_address}"
+  value = digitalocean_droplet.mywebserver.ipv4_address
 }
 
 output "IPv6" {
-  value = "${digitalocean_droplet.mywebserver.ipv6_address}"
+  value = digitalocean_droplet.mywebserver.ipv6_address
 }
 
 output "Name" {
-  value = "${digitalocean_droplet.mywebserver.name}"
+  value = digitalocean_droplet.mywebserver.name
 }
