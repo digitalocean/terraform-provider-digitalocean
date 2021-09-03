79e41814a13d2630b7fb0ea2c883b045226345b0
Mads Jensen <mads.jensen@eficode.com>
2021-09-03 14:37:26 +0200

    Temlate for testing

--- digitalocean/resource_digitalocean_monitor_alert_test.go
+++ digitalocean/resource_digitalocean_monitor_alert_test.go
@@ -3,9 +3,12 @@ package digitalocean
 import (
 	"fmt"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func testAccAlertPolicy(withAlerts bool) string {
+func testAccAlertPolicy(window string) string {
 	return fmt.Sprint(`
 resource "digitalocean_droplet" "web" {
 	image  = "ubuntu-20-04-x64"
@@ -21,7 +24,7 @@ resource "digitalocean_droplet" "web" {
 		"channel"   = "Production Alerts",
 		"url"       = "https://hooks.slack.com/services/T1234567/AAAAAAAA/ZZZZZZ"
 	}
-	window      = "5m"
+	window      = %s
 	type        = "v1/insights/droplet/cpu"
 	compare     = "GreaterThan"
 	value       = 95
@@ -29,9 +32,30 @@ resource "digitalocean_droplet" "web" {
 	entities    = [digitalocean_droplet.web.id]
 	description = "Alert about CPU usage"
   }
-`)
+`, window)
 }
 
 func TestAccDigitalOceanMonitorAlert(t *testing.T) {
+	rInt := acctest.RandInt()
 
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                  func() { testAccPreCheck(t) },
+		ProviderFactories:         testAccProviderFactories,
+		PreventPostDestroyRefresh: true,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceDigitalOceanSpacesObjectsConfigResources(rInt), // NOTE: contains no data source
+				// Does not need Check
+			},
+			{
+				Config: testAccDataSourceDigitalOceanSpacesObjectsConfigMaxKeys(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDigitalOceanSpacesObjectsDataSourceExists("data.digitalocean_spaces_bucket_objects.yesh"),
+					resource.TestCheckResourceAttr("data.digitalocean_spaces_bucket_objects.yesh", "keys.#", "2"),
+					resource.TestCheckResourceAttr("data.digitalocean_spaces_bucket_objects.yesh", "keys.0", "arch/courthouse_towers/landscape"),
+					resource.TestCheckResourceAttr("data.digitalocean_spaces_bucket_objects.yesh", "keys.1", "arch/navajo/north_window"),
+				),
+			},
+		},
+	})
 }
