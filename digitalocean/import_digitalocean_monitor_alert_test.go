f69c6d6ed58344b76b65e1a9807ea433c13d14e6
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2021-09-21 11:02:31 -0400

    Fix import test.

--- digitalocean/import_digitalocean_monitor_alert_test.go
+++ digitalocean/import_digitalocean_monitor_alert_test.go
@@ -6,12 +6,9 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccDigitalOceanMonitorAlert_importBasic(t *testing.T) {
-	// copied from the database import test, but 3/3 does not fail ...
-
 	randName := randomTestName()
 	resourceName := fmt.Sprintf("digitalocean_monitor_alert.%s", randName)
 
@@ -27,7 +24,6 @@ func TestAccDigitalOceanMonitorAlert_importBasic(t *testing.T) {
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
-				ImportStateIdFunc: testAccMonitorAlertImportID(resourceName),
 			},
 			// Test importing non-existent resource provides expected error.
 			{
@@ -35,22 +31,8 @@ func TestAccDigitalOceanMonitorAlert_importBasic(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: false,
 				ImportStateId:     "this-monitor-alert-id-does-not-exist",
-				ExpectError:       regexp.MustCompile(`some fancy error`),
+				ExpectError:       regexp.MustCompile(`Please verify the ID is correct|Cannot import non-existent remote object`),
 			},
 		},
 	})
-}
-
-func testAccMonitorAlertImportID(n string) resource.ImportStateIdFunc {
-	return func(s *terraform.State) (string, error) {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return "", fmt.Errorf("Not found: %s", n)
-		}
-
-		monitorAlertID := rs.Primary.Attributes["monitor_alert_id"]
-		name := rs.Primary.Attributes["name"]
-
-		return fmt.Sprintf("%s,%s", monitorAlertID, name), nil
-	}
 }
