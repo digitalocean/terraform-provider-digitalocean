digitalocean/terraform-provider-digitalocean

commit 35e97db320ce68e6ff924e5d75e8e2b66854b501

Simplified some loops based on go-staticcheck S1011 (#656)
* Simplified some loops based on go-staticcheck S1011

More details about the pattern are given in https://staticcheck.io/docs/checks#S1011

* Simplified S1031.

digitalocean/datasource_digitalocean_tags.go
@@ -58,9 +58,7 @@ func getDigitalOceanTags(meta interface{}, extra map[string]interface{}) ([]inte
 			return nil, fmt.Errorf("Error retrieving tags: %s", err)
 		}
 
-		for _, tag := range tags {
-			tagsList = append(tagsList, tag)
-		}
+		tagsList = append(tagsList, tags)
 
 		if resp.Links == nil || resp.Links.IsLastPage() {
 			break

digitalocean/datasource_digitalocean_database_cluster.go
@@ -138,9 +138,7 @@ func dataSourceDigitalOceanDatabaseClusterRead(ctx context.Context, d *schema.Re
 			return diag.Errorf("Error retrieving DatabaseClusters: %s", err)
 		}
 
-		for _, d := range databases {
-			databaseList = append(databaseList, d)
-		}
+		databaseList = append(databaseList, databases...)
 
 		if resp.Links == nil || resp.Links.IsLastPage() {
 			break

digitalocean/datasource_digitalocean_droplet_snapshot.go
@@ -92,9 +92,7 @@ func dataSourceDigitalOceanDropletSnapshotRead(ctx context.Context, d *schema.Re
 			return diag.Errorf("Error retrieving Droplet snapshots: %s", err)
 		}
 
-		for _, s := range snapshots {
-			snapshotList = append(snapshotList, s)
-		}
+		snapshotList = append(snapshotList, snapshots...)
 
 		if resp.Links == nil || resp.Links.IsLastPage() {
 			break

digitalocean/datasource_digitalocean_loadbalancer.go
@@ -220,9 +220,7 @@ func dataSourceDigitalOceanLoadbalancerRead(ctx context.Context, d *schema.Resou
 			return diag.Errorf("Error retrieving load balancers: %s", err)
 		}
 
-		for _, lb := range lbs {
-			lbList = append(lbList, lb)
-		}
+		lbList = append(lbList, lbs...)
 
 		if resp.Links == nil || resp.Links.IsLastPage() {
 			break

digitalocean/datasource_digitalocean_volume.go
@@ -95,9 +95,7 @@ func dataSourceDigitalOceanVolumeRead(ctx context.Context, d *schema.ResourceDat
 			return diag.Errorf("Error retrieving volumes: %s", err)
 		}
 
-		for _, volume := range volumes {
-			volumeList = append(volumeList, volume)
-		}
+		volumeList = append(volumeList, volumes...)
 
 		if resp.Links == nil || resp.Links.IsLastPage() {
 			break

digitalocean/datasource_digitalocean_volume_snapshot.go
@@ -96,9 +96,7 @@ func dataSourceDigitalOceanVolumeSnapshotRead(ctx context.Context, d *schema.Res
 			return diag.Errorf("Error retrieving volume snapshots: %s", err)
 		}
 
-		for _, s := range snapshots {
-			snapshotList = append(snapshotList, s)
-		}
+		snapshotList = append(snapshotList, snapshots...)
 
 		if resp.Links == nil || resp.Links.IsLastPage() {
 			break

digitalocean/datasource_digitalocean_vpc.go
@@ -122,9 +122,7 @@ func listVPCs(client *godo.Client) ([]*godo.VPC, error) {
 			return vpcList, fmt.Errorf("Error retrieving VPCs: %s", err)
 		}
 
-		for _, v := range vpcs {
-			vpcList = append(vpcList, v)
-		}
+		vpcList = append(vpcList, vpcs...)
 
 		if resp.Links == nil || resp.Links.IsLastPage() {
 			break

digitalocean/kubernetes.go
@@ -182,20 +182,16 @@ func nodeSchema() *schema.Schema {
 
 func expandLabels(labels map[string]interface{}) map[string]string {
 	expandedLabels := make(map[string]string)
-	if labels != nil {
-		for key, value := range labels {
-			expandedLabels[key] = value.(string)
-		}
+	for key, value := range labels {
+		expandedLabels[key] = value.(string)
 	}
 	return expandedLabels
 }
 
 func flattenLabels(labels map[string]string) map[string]interface{} {
 	flattenedLabels := make(map[string]interface{})
-	if labels != nil {
-		for key, value := range labels {
-			flattenedLabels[key] = value
-		}
+	for key, value := range labels {
+		flattenedLabels[key] = value
 	}
 	return flattenedLabels
 }

commit a2a5c3a9d5b3948c82fd45ddc496e095b53ff2e8

Fix tag collection in digitalocean_tags data source
Fixes regression introduced in 35e97db3.

See-also: 35e97db3 (Simplified some loops based on go-staticcheck S1011 (#656), 2021-07-13)

digitalocean/datasource_digitalocean_tags.go
@@ -58,7 +58,9 @@ func getDigitalOceanTags(meta interface{}, extra map[string]interface{}) ([]inte
 			return nil, fmt.Errorf("Error retrieving tags: %s", err)
 		}
 
-		tagsList = append(tagsList, tags)
+		for _, tag := range tags {
+			tagsList = append(tagsList, tag)
+		}
 
 		if resp.Links == nil || resp.Links.IsLastPage() {
 			break