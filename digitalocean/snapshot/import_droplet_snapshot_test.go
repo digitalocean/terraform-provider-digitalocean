c3452f2467f5d43788b328fe369fa505664306a7
Andrew Starr-Bochicchio <andrewsomething@users.noreply.github.com>
2023-02-10 13:00:03 -0500

    Use consistent naming for all snapshots created in tests. (#938)
    * Use consistent naming for all volume snapshots created in tests.
    
    * Use consistent naming for all droplet snapshots created in tests.
    
    * Use consistent naming for all snapshots created in image tests.

--- digitalocean/snapshot/import_droplet_snapshot_test.go
+++ digitalocean/snapshot/import_droplet_snapshot_test.go
@@ -5,14 +5,13 @@ import (
 	"testing"
 
 	"github.com/digitalocean/terraform-provider-digitalocean/digitalocean/acceptance"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccDigitalOceanDropletSnapshot_importBasic(t *testing.T) {
 	resourceName := "digitalocean_droplet_snapshot.foobar"
 	dName := acceptance.RandomTestName()
-	rInt2 := acctest.RandInt()
+	snapName := acceptance.RandomTestName()
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
@@ -20,7 +19,7 @@ func TestAccDigitalOceanDropletSnapshot_importBasic(t *testing.T) {
 		CheckDestroy:      testAccCheckDigitalOceanVolumeSnapshotDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(testAccCheckDigitalOceanDropletSnapshotConfig_basic, dName, rInt2),
+				Config: fmt.Sprintf(testAccCheckDigitalOceanDropletSnapshotConfig_basic, dName, snapName),
 			},
 
 			{
