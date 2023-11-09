84d52d4ee59618368a9135f458d10c5e62e13daf
T-jegou <thomas.jegou1@gmail.com>
2023-11-09 16:00:25 +0100

    Add database connection pool datasource

---
+++ digitalocean/database/datasource_database_connection_pool.go
@@ -0,0 +1,93 @@
+package database
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func DataSourceDigitalOceanDatabaseConnectionPool() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceDigitalOceanDatabaseConnectionPoolRead,
+		Schema: map[string]*schema.Schema{
+			"cluster_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.NoZeroValues,
+			},
+
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(3, 63),
+			},
+
+			"user": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"mode": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"size": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"db_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"host": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"private_host": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"port": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+
+			"uri": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+
+			"private_uri": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+
+			"password": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+		},
+	}
+
+}
+
+func dataSourceDigitalOceanDatabaseConnectionPoolRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+
+	clusterID := d.Get("cluster_id").(string)
+	poolName := d.Get("name").(string)
+	d.SetId(createConnectionPoolID(clusterID, poolName))
+
+	return resourceDigitalOceanDatabaseConnectionPoolRead(ctx, d, meta)
+}
