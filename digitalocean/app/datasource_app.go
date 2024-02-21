81c1c3a1eeb6d6ca837ccd395c4c88e2fd83625c
danaelhe <42972711+danaelhe@users.noreply.github.com>
2024-02-21 10:11:53 -0500

    remove unused package

--- digitalocean/app/datasource_app.go
+++ digitalocean/app/datasource_app.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
 func DataSourceDigitalOceanApp() *schema.Resource {
