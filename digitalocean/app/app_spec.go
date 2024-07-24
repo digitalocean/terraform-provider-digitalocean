39d4207c28fb852ccd44026cc3477ffe95a2ce8e
Andrew Starr-Bochicchio <andrewsomething@users.noreply.github.com>
2024-07-24 16:34:45 -0400

    apps: support additional database engines (#1182)

--- digitalocean/app/app_spec.go
+++ digitalocean/app/app_spec.go
@@ -896,6 +896,8 @@ func appSpecDatabaseSchema() *schema.Resource {
 					"PG",
 					"REDIS",
 					"MONGODB",
+					"KAFKA",
+					"OPENSEARCH",
 				}, false),
 				Description: "The database engine to use.",
 			},
