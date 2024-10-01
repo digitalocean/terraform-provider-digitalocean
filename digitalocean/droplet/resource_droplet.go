a5af2a38c64ddb5bc071614c9fba9340939de72f
TheMarvelFan <nigampranshu665@gmail.com>
2024-10-01 18:44:33 +0530

    Uncommented the linting errcheck in '.golangci.yml' and fixed linting issues in several files.

--- digitalocean/droplet/resource_droplet.go
+++ digitalocean/droplet/resource_droplet.go
@@ -663,7 +663,10 @@ func resourceDigitalOceanDropletUpdate(ctx context.Context, d *schema.ResourceDa
 			}
 		}
 		for volumeID := range leftDiff(oldIDSet, newIDSet) {
-			detachVolumeIDOnDroplet(d, volumeID, meta)
+			err := detachVolumeIDOnDroplet(d, volumeID, meta)
+			if err != nil {
+				return nil
+			}
 		}
 	}
 
@@ -849,7 +852,10 @@ func detachVolumesFromDroplet(d *schema.ResourceData, meta interface{}) error {
 	if attr, ok := d.GetOk("volume_ids"); ok {
 		errors = make([]error, 0, attr.(*schema.Set).Len())
 		for _, volumeID := range attr.(*schema.Set).List() {
-			detachVolumeIDOnDroplet(d, volumeID.(string), meta)
+			err := detachVolumeIDOnDroplet(d, volumeID.(string), meta)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
