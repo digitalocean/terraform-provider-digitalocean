73a08ed06482164804bd9743d1fa197c276940f5
Rémy Sanchez <remy.sanchez@hyperthese.net>
2025-01-23 22:53:03 +0100

    There is no minimum on services (#1305)
    * There is no minimum on services
    
    * Remove MinItems altogether.
    
    ---------
    
    Co-authored-by: Andrew Starr-Bochicchio <andrewsomething@users.noreply.github.com>
    Co-authored-by: Andrew Starr-Bochicchio <a.starr.b@gmail.com>

--- digitalocean/app/app_spec.go
+++ digitalocean/app/app_spec.go
@@ -63,7 +63,6 @@ func appSpecSchema(isResource bool) map[string]*schema.Schema {
 		"service": {
 			Type:     schema.TypeList,
 			Optional: true,
-			MinItems: 1,
 			Elem:     appSpecServicesSchema(),
 		},
 		"static_site": {
