8f36cfde04562363c93c89bd07bb05767f7b17ed
Aryan <paryan@digitalocean.com>
2025-03-25 13:06:27 +0000

    Use proper schema object type

--- digitalocean/app/app_spec.go
+++ digitalocean/app/app_spec.go
@@ -207,7 +207,7 @@ func appSpecAppLevelAlerts() *schema.Resource {
 				Optional: true,
 			},
 			"destinations": {
-				Type:     schema.TypeList,
+				Type:     schema.TypeSet,
 				Optional: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -990,7 +990,7 @@ func appSpecComponentAlerts() *schema.Resource {
 				Optional: true,
 			},
 			"destinations": {
-				Type:     schema.TypeList,
+				Type:     schema.TypeSet,
 				Optional: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
