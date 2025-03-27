e409193b16541ed1f52a181d3e958d7e5ce5709d
Alberto Pinon Formoso <apinonformoso@digitalocean.com>
2025-03-27 10:38:42 -0600

    introduction of partner network connect

---
+++ digitalocean/partnernetworkconnect/datasource_partner_network_connect_test.go
@@ -0,0 +1,160 @@
+package partnernetworkconnect_test
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/digitalocean/godo"
+	"github.com/digitalocean/terraform-provider-digitalocean/digitalocean/acceptance"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceDigitalOceanPartnerAttachment_ByID(t *testing.T) {
+	var partnerAttachment godo.PartnerAttachment
+	partnerAttachmentName := acceptance.RandomTestName()
+	vpcName1 := acceptance.RandomTestName()
+	vpcName2 := acceptance.RandomTestName()
+	resourceConfig := fmt.Sprintf(testAccCheckDataSourceDigitalOceanPartnerAttachmentConfig_Basic, vpcName1, vpcName2, partnerAttachmentName)
+	dataSourceConfig := `
+data "digitalocean_partner_attachment" "foobar" {
+  id = digitalocean_partner_attachment.foobar.id
+}`
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: resourceConfig,
+			},
+			{
+				Config: resourceConfig + dataSourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDigitalOceanPartnerAttachmentExists("data.digitalocean_partner_attachment.foobar", &partnerAttachment),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "name", partnerAttachmentName),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "connection_bandwidth_in_mbps", "1000"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "region", "nyc"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "naas_provider", "MEGAPORT"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "vpc_ids.#", "2"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "bgp.0.local_router_ip", "169.254.100.1/29"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "bgp.0.peer_router_asn", "133937"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "bgp.0.peer_router_ip", "169.254.100.6/29"),
+					resource.TestCheckResourceAttrSet(
+						"data.digitalocean_partner_attachment.foobar", "created_at"),
+					resource.TestCheckResourceAttrSet(
+						"data.digitalocean_partner_attachment.foobar", "state"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceDigitalOceanPartnerAttachment_ByName(t *testing.T) {
+	var partnerAttachment godo.PartnerAttachment
+	partnerAttachmentName := acceptance.RandomTestName()
+	vpcName1 := acceptance.RandomTestName()
+	vpcName2 := acceptance.RandomTestName()
+	resourceConfig := fmt.Sprintf(testAccCheckDataSourceDigitalOceanPartnerAttachmentConfig_Basic, vpcName1, vpcName2, partnerAttachmentName)
+	dataSourceConfig := `
+data "digitalocean_partner_attachment" "foobar" {
+  name = digitalocean_partner_attachment.foobar.name
+}`
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: resourceConfig,
+			},
+			{
+				Config: resourceConfig + dataSourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDigitalOceanPartnerAttachmentExists("data.digitalocean_partner_attachment.foobar", &partnerAttachment),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "name", partnerAttachmentName),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "connection_bandwidth_in_mbps", "1000"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "region", "nyc"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "naas_provider", "MEGAPORT"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "vpc_ids.#", "2"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "bgp.0.local_router_ip", "169.254.100.1/29"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "bgp.0.peer_router_asn", "133937"),
+					resource.TestCheckResourceAttr(
+						"data.digitalocean_partner_attachment.foobar", "bgp.0.peer_router_ip", "169.254.100.6/29"),
+					resource.TestCheckResourceAttrSet(
+						"data.digitalocean_partner_attachment.foobar", "created_at"),
+					resource.TestCheckResourceAttrSet(
+						"data.digitalocean_partner_attachment.foobar", "state"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceDigitalOceanPartnerAttachment_ExpectErrors(t *testing.T) {
+	partnenrAttachmentName := acceptance.RandomTestName()
+	partnerAttachmentNotExists := fmt.Sprintf(testAccCheckDataSourceDigitalOceanPartnerAttachmentConfig_DoesNotExist, partnenrAttachmentName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      partnerAttachmentNotExists,
+				ExpectError: regexp.MustCompile(`no Partner Attachment found with name`),
+			},
+		},
+	})
+}
+
+const testAccCheckDataSourceDigitalOceanPartnerAttachmentConfig_Basic = `
+resource "digitalocean_vpc" "vpc1" {
+  name   = "%s"
+  region = "nyc3"
+}
+resource "digitalocean_vpc" "vpc2" {
+  name   = "%s"
+  region = "nyc3"
+}
+resource "digitalocean_partner_attachment" "foobar" {
+  name                         = "%s"
+  connection_bandwidth_in_mbps = 1000
+  region                       = "nyc"
+  naas_provider                = "MEGAPORT"
+  vpc_ids = [
+    digitalocean_vpc.vpc1.id,
+    digitalocean_vpc.vpc2.id
+  ]
+  depends_on = [
+    digitalocean_vpc.vpc1,
+    digitalocean_vpc.vpc2
+  ]
+  bgp {
+    local_router_ip = "169.254.100.1/29"
+    peer_router_asn = 133937
+    peer_router_ip  = "169.254.100.6/29"
+    auth_key        = "BGPAu7hK3y!"
+  }
+}
+`
+
+const testAccCheckDataSourceDigitalOceanPartnerAttachmentConfig_DoesNotExist = `
+data "digitalocean_partner_attachment" "foobar" {
+  name = "%s"
+}
+`
---
+++ docs/data-sources/partner_network_connect.md
@@ -0,0 +1,57 @@
+---
+page_title: "DigitalOcean: digitalocean_partner_attachment"
+subcategory: "Networking"
+---
+
+# digitalocean_partner_attachment
+
+-> Partner Attachment is currently in private preview. If you are not a member of the private preview group for this feature, you will not be able to use it until is has been more widely released. Please follow the official [DigitalOcean changelog](https://docs.digitalocean.com/release-notes/) for updates.
+
+Retrieve information about a Partner Attachment for use in other resources.
+
+This data source provides all of the Partner Attachment's properties as configured on your
+DigitalOcean account. This is useful if the Partner Attachment in question is not managed by
+Terraform or you need to utilize any of the Partner Attachment's data.
+
+Partner Attachment may be looked up by `id` or `name`.
+
+## Example Usage
+
+### Partner Attachment By Id
+
+```hcl
+data "digitalocean_partner_attachment" "example" {
+  id = "example-id"
+}
+```
+
+### Partner Attachment By Name
+
+```hcl
+data "digitalocean_partner_attachment" "example" {
+  name = "example-pia"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported and are mutually exclusive:
+
+* `id` - The unique identifier of an existing Partner Attachment.
+* `name` - The name of an existing Partner Attachment.
+
+## Attributes Reference
+
+* `id` - The unique identifier of an existing Partner Attachment.
+* `name` - The name of the Partner Attachment.
+* `connection_bandwidth_in_mbps` - The bandwidth in megabits per second of the connection.
+* `region` - The region where the Partner Attachment is located.
+* `naas_provider` - The network as a service provider for the Partner Attachment.
+* `vpc_ids` - The list of VPC IDs involved in the Partner Attachment.
+* `bgp` - The BGP configuration for the Partner Attachment.
+    * `local_router_ip` - The local router IP address in CIDR notation.
+    * `peer_router_asn` - The peer autonomous system number.
+    * `peer_router_ip` - The peer router IP address in CIDR notation.
+    * `auth_key` - The authentication key for the BGP session.
+* `state` - The state of the Partner Attachment.
+* `created_at` - The date and time of when the Partner Attachment was created.
