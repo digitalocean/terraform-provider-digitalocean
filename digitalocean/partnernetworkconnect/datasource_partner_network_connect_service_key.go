097202b7d503569eef072fa83292b9f920bd01c7
Joe Keegan <jkeegan@digitalocean.com>
2025-06-23 16:48:15 -0400

    fixed lint

--- digitalocean/partnernetworkconnect/datasource_partner_network_connect_service_key.go
+++ digitalocean/partnernetworkconnect/datasource_partner_network_connect_service_key.go
@@ -2,6 +2,7 @@ package partnernetworkconnect
 
 import (
 	"context"
+
 	"github.com/digitalocean/terraform-provider-digitalocean/digitalocean/config"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
