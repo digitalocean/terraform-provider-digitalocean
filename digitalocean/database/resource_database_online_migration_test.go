239ffdf01934d7d7d0bc21549b2907d55cfae75e
danaelhe <42972711+danaelhe@users.noreply.github.com>
2025-08-04 11:41:42 -0400

    Databases: Add online migration support (#1331)
    * Databases: Add online migration support
    
    * update required attributes
    
    * reorder packages
    
    * clean up
    
    * terrafmt
    
    * terrafmt
    
    * Update docs/resources/database_online_migration.md
    
    Co-authored-by: kishlay-singh-DO <ksingh@digitalocean.com>
    
    * Update digitalocean/database/resource_database_online_migration.go
    
    Co-authored-by: kishlay-singh-DO <ksingh@digitalocean.com>
    
    ---------
    
    Co-authored-by: kishlay-singh-DO <ksingh@digitalocean.com>

---
+++ digitalocean/database/resource_database_online_migration_test.go
@@ -0,0 +1,68 @@
+package database_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/digitalocean/terraform-provider-digitalocean/digitalocean/acceptance"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDigitalOceanDatabaseOnlineMigration_Basic(t *testing.T) {
+	source := "source-" + acceptance.RandomTestName()
+	destination := "destination-" + acceptance.RandomTestName()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		//CheckDestroy:      testAccCheckDigitalOceanDatabaseClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccCheckDigitalOceanDatabaseOnlineMigrationBasic, source, "8", destination, "8"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("digitalocean_database_online_migration.foobar", "id"),
+					resource.TestCheckResourceAttrSet("digitalocean_database_online_migration.foobar", "status"),
+					resource.TestCheckResourceAttrSet("digitalocean_database_online_migration.foobar", "created_at"),
+				),
+			},
+		},
+	})
+}
+
+const testAccCheckDigitalOceanDatabaseOnlineMigrationBasic = `
+resource "digitalocean_database_cluster" "source" {
+  name       = "%s"
+  engine     = "mysql"
+  version    = "%s"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
+  tags       = ["production"]
+}
+
+resource "digitalocean_database_cluster" "destination" {
+  name       = "%s"
+  engine     = "mysql"
+  version    = "%s"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
+  tags       = ["production"]
+}
+
+resource "digitalocean_database_db" "source_db" {
+  cluster_id = digitalocean_database_cluster.source.id
+  name       = "terraform-db-om-source"
+}
+
+resource "digitalocean_database_online_migration" "foobar" {
+  cluster_id = digitalocean_database_cluster.destination.id
+  source {
+    host     = digitalocean_database_cluster.source.host
+    db_name  = digitalocean_database_db.source_db.name
+    port     = digitalocean_database_cluster.source.port
+    username = digitalocean_database_cluster.source.user
+    password = digitalocean_database_cluster.source.password
+  }
+  depends_on = [digitalocean_database_cluster.destination, digitalocean_database_cluster.source, digitalocean_database_db.source_db]
+}`
