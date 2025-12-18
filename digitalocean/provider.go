d10004bca45d74ce04d201400f800d690f7403fd
Joe Keegan <jkeegan@digitalocean.com>
2025-12-18 09:31:33 -0500

    Added digitalocean_database_logsink_rsyslog and digitalocean_database_logsink_opensearch Resources (#1474)

--- digitalocean/provider.go
+++ digitalocean/provider.go
@@ -197,6 +197,8 @@ func Provider() *schema.Provider {
 			"digitalocean_database_kafka_topic":                  database.ResourceDigitalOceanDatabaseKafkaTopic(),
 			"digitalocean_database_kafka_schema_registry":        database.ResourceDigitalOceanDatabaseKafkaSchemaRegistry(),
 			"digitalocean_database_online_migration":             database.ResourceDigitalOceanDatabaseOnlineMigration(),
+			"digitalocean_database_logsink_rsyslog":              database.ResourceDigitalOceanDatabaseLogsinkRsyslog(),
+			"digitalocean_database_logsink_opensearch":           database.ResourceDigitalOceanDatabaseLogsinkOpensearch(),
 			"digitalocean_domain":                                domain.ResourceDigitalOceanDomain(),
 			"digitalocean_droplet":                               droplet.ResourceDigitalOceanDroplet(),
 			"digitalocean_droplet_autoscale":                     dropletautoscale.ResourceDigitalOceanDropletAutoscale(),
