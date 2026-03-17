6da7f13748cfa8c65d484062c19761fd3ab7c0a7
pyadagiri <pyadagiri@digitalocean.com>
2026-03-17 12:20:28 +0530

    godo vendor changes

--- vendor/github.com/digitalocean/godo/kubernetes.go
+++ vendor/github.com/digitalocean/godo/kubernetes.go
@@ -76,8 +76,8 @@ type KubernetesClusterCreateRequest struct {
 	ClusterSubnet string   `json:"cluster_subnet,omitempty"`
 	ServiceSubnet string   `json:"service_subnet,omitempty"`
 
-	// Create cluster with highly available control plane
-	HA bool `json:"ha"`
+	// Create cluster with highly available control plane. When nil, API applies version-dependent default (true for 1.36+).
+	HA *bool `json:"ha,omitempty"`
 
 	NodePools []*KubernetesNodePoolCreateRequest `json:"node_pools,omitempty"`
 
